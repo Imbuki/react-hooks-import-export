964377ea5ab9bee36e8930c42f68415043649e58
Imbuki <+>
2023-07-22 13:27:05 +0300

    done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,21 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as myFunctions from "./parks/RockyMountain";
+import MesaVerde from "./parks/MesaVerde";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  myFunctions.wildlife();
+  myFunctions.elevation()
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+  <div>
+     <h1>Colorado State Parks!</h1>
+     <MesaVerde />
+  </div>
+ 
+
+  );
 }
+
+export default ColoradoStateParks
